f5f42b0d5208d9fa8b54db3ddeadeacd41da40e6
Tomoki Morita <tmsongbooks215@gmail.com>
2023-01-06 22:31:40 +0900

    remove unused some components

--- src/components/title.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react-native";
-import { Title } from "./title";
-
-test("", () => {
-  render(<Title>foo</Title>);
-  expect(screen.getByText("foo")).toBeTruthy();
-});
