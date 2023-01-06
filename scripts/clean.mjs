9180b150806f2cb8b7af7a211f9656ea161e0989
Tomoki Morita <tmsongbooks215@gmail.com>
2023-01-06 21:11:43 +0900

    update some scripts

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -1,5 +1,6 @@
 import { rm } from "node:fs/promises";
 
 await rm(".expo", { recursive: true, force: true });
+await rm("coverage", { recursive: true, force: true });
 await rm("dist", { recursive: true, force: true });
 await rm("node_modules", { recursive: true, force: true });
--- scripts/clear.mjs
+++ scripts/clear.mjs
@@ -1,4 +1,5 @@
 import { rm } from "node:fs/promises";
 
 await rm(".expo", { recursive: true, force: true });
+await rm("coverage", { recursive: true, force: true });
 await rm("dist", { recursive: true, force: true });
