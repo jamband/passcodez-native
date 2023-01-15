02d8d485e7268968d0d1390e03fb28123b3eb17c
Tomoki Morita <tmsongbooks215@gmail.com>
2023-01-15 19:50:52 +0900

    add app.config.ts and more
    update .gitignore
    use dotenv
    create eas.json for ios simulator preview

---
+++ app.config.ts
@@ -0,0 +1,41 @@
+import * as dotenv from "dotenv";
+import type { ConfigContext, ExpoConfig } from "expo/config";
+
+dotenv.config();
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "Passcodez",
+  slug: "passcodez",
+  version: "0.1.0",
+  orientation: "portrait",
+  icon: "./src/assets/icon.png",
+  userInterfaceStyle: "dark",
+  splash: {
+    image: "./src/assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#13111a",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.jamband.passcodez",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./src/assets/adaptive-icon.png",
+      backgroundColor: "#13111A",
+    },
+  },
+  web: {
+    favicon: "./src/assets/favicon.png",
+  },
+  extra: {
+    eas: {
+      projectId: process.env.EAS_PROJECT_ID,
+    },
+  },
+});
