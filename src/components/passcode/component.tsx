9886a911ac0192ea81b65e7c459692e88aac0d39
Tomoki Morita <tmsongbooks215@gmail.com>
2025-03-10 16:00:25 +0900

    add some biome ignores

--- src/components/passcode/component.tsx
+++ src/components/passcode/component.tsx
@@ -6,6 +6,7 @@ import type { _Props } from "./types";
 export const Component: React.FC<_Props> = (props) => (
   <View style={styles.container}>
     {[...Array(PASSCODE_LENGTH)].map((_, index) => (
+      // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
       <Text key={index} style={styles.number}>
         {props.passcode[index]}
       </Text>
--- src/components/pressed-code/component.tsx
+++ src/components/pressed-code/component.tsx
@@ -7,6 +7,7 @@ export const Component: React.FC<_Props> = (props) => (
   <View style={styles.container}>
     {[...Array(PASSCODE_LENGTH)].map((_, index) => (
       <View
+        // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
         key={index}
         style={[
           styles.number,
